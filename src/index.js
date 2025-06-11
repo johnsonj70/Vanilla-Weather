13d25fafb700b7d4948fab750fe0cb13bfafac89
Janice <johnson.janice30@yahoo.co.uk>
2025-06-10 22:26:13 -0400

    Added basic search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-alert('Hello World!');
+function search(event) {
+	event.preventDefault();
+	let searchInputElement = document.querySelector('#search-form-input');
+	searchForCity(searchInputElement.value);
+}
